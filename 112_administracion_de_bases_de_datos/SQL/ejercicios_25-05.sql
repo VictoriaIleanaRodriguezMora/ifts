9e2c7ffdf39839f5e14b8bdf072497c1c9074f19
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2025-06-05 19:12:13 -0300

    feat: Ejercicios 5, 6, 7 y 8. Archivo .git para extension de .sql

--- 112_administracion_de_bases_de_datos/SQL/ejercicios_25-05.sql
+++ 112_administracion_de_bases_de_datos/SQL/ejercicios_25-05.sql
@@ -1,8 +1,9 @@
 USE comercio_electronico;
 # ✅ 3 . Obtener el nombre del cliente que realizó la venta con idVenta = 2:
--- este intento me devolvió algo pero no estaba correcto
+/* -- este intento me devolvió algo pero no estaba correcto
 -- SELECT ventas.idCliente FROM ventas RIGHT JOIN clientes ON ventas.idCliente WHERE ventas.idCliente = 2 AND clientes.idCliente = 2;
-/*
+*/
+/* -- EJ 03
 SELECT clientes.nombreCliente FROM clientes 
 LEFT JOIN ventas
 ON ventas.idCliente = clientes.idCliente
@@ -17,14 +18,7 @@ https://stackoverflow.com/questions/8084571/not-unique-table-alias
 */
 
 # 4. Obtener el total de ventas realizadas por cada cliente:
--- ¿De compras?
--- Clientes - idCliente | Ventas - idVenta | 
--- ventas_detalle - idVentaDetalle, idVenta, idProducto, cantidad
--- productos | idProducto, precioCompra, precioVenta
-/*(SELECT clientes.nombreCliente FROM clientes 
-LEFT JOIN ventas
-ON ventas.idCliente = clientes.idCliente)
-UNION */
+/* -- EJERCICIO 04
 SELECT 
 -- vd.idVentaDetalle,
  vd.idVenta
@@ -36,7 +30,19 @@ JOIN productos AS pr
 ON vd.idProducto = pr.idProducto
 GROUP BY vd.idVenta
 ;
-/* NOTAS PARA EL EJERCICIO # 044
+*/
+
+-- ¿De compras?
+-- Clientes - idCliente | Ventas - idVenta | 
+-- ventas_detalle - idVentaDetalle, idVenta, idProducto, cantidad
+-- productos | idProducto, precioCompra, precioVenta
+
+/*(SELECT clientes.nombreCliente FROM clientes 
+LEFT JOIN ventas
+ON ventas.idCliente = clientes.idCliente)
+UNION */
+
+/* NOTAS PARA EL EJERCICIO # 04
 ¿Qué es una columna agregada? "nonagreggated column"
 Es una columna que se procesa usando una función de agregación, como:
 SUM(), AVG, COUNT, MAX, MIN, 
@@ -63,7 +69,7 @@ Entonces: Cada columna que aparece en el SELECT debe estar:
 
 
 */
-/*
+/* ERRORES que obtuve
 Error Code: 1222. The used SELECT statements have a different number of columns
 Error Code: 1055. Expression #2 of SELECT list is not in GROUP BY clause and contains nonaggregated column 'comercio_electronico.vd.cantidad' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by
 ELECT vd.idVenta, vd.cantidad, pr.precioVenta,
@@ -107,7 +113,7 @@ GROUP BY vd.idVentaDetalle
 
 # 5. Obtener todos los productos de la categoría "Electrónica" con su nombre de categoría:
 # (Hecho en clase)
-/*
+/* -- EJ 05
 SELECT * -- trae TODO de todas las tablas
 	FROM ventas_detalle 
 INNER JOIN productos
@@ -121,15 +127,57 @@ WHERE ventas_detalle.idVenta = 1
 ;
 */
 # 6. Obtener el nombre y la dirección de todos los clientes que han realizado ventas:
+/* -- EJ 06
 SELECT ventas.idVenta, clientes.nombreCliente, clientes.direccionCliente
 	FROM ventas
 INNER JOIN clientes -- INNER JOIN pq necesito unicamente los clientes con ventas. no ventas sin clientes
 	ON ventas.idCliente = clientes.idCliente
-
-
+*/
 
 # 7. Obtener los detalles de ventas de la venta con idVenta = 1, incluyendo el nombre del producto y su precio de venta:
-# 8.Obtener el total de ventas realizadas por cada cliente, mostrando el nombre del cliente y la cantidad total de ventas:
+/* -- EJ 07
+SELECT v.idVenta
+, c.nombreCliente
+, c.direccionCliente
+-- , vd.idVentaDetalle
+-- , p.idProducto
+, p.nombreProducto
+, p.precioVenta
+	FROM ventas AS v
+INNER JOIN clientes AS c -- INNER JOIN pq necesito unicamente los clientes con ventas. no ventas sin clientes
+	ON v.idCliente = c.idCliente
+INNER JOIN ventas_detalle AS vd
+	ON vd.idVenta = v.idVenta
+INNER JOIN productos AS p
+	ON vd.idProducto = p.idProducto
+		WHERE v.idVenta = 1
+;
+*/
+
+# 8. Obtener el total de ventas realizadas por cada cliente, mostrando el nombre del cliente y la cantidad total de ventas:
+/* -- EJERCICIO 08
+SELECT 
+-- v.idVenta,
+ c.nombreCliente
+-- , c.direccionCliente
+-- , p.nombreProducto
+-- , p.precioVenta
+, SUM(p.precioVenta * cantidad) AS 'suma de las ventas de cada cliente'
+# JUAN compró: 2 iphone de 1000 c/u y 1 macbook pro de 2000 = 4000
+# María compró; 3 Airpods pro de 250 c/u y 2 pantalon azul de 45 = 840
+	FROM ventas AS v
+INNER JOIN clientes AS c 
+	ON v.idCliente = c.idCliente
+INNER JOIN ventas_detalle AS vd
+	ON vd.idVenta = v.idVenta
+INNER JOIN productos AS p
+	ON vd.idProducto = p.idProducto
+GROUP BY (c.nombreCliente)
+;
+*/
+
 # 9. Obtener los nombres de las categorías y la cantidad de productos vendidos en cada categoría:
+SELECT * FROM productos
+;
 # 10. Obtener el nombre de cada cliente y el total gastado en compras por cada cliente, incluyendo aquellos clientes que no han realizado compras:
 
